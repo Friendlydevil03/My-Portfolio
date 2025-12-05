2af3b006407974ead6439544fb55756a21a5ac1a
Srivenkatesh R <117419799+Srivenkatesh03@users.noreply.github.com>
2025-12-06 00:11:28 +0530

    Update Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -12,7 +12,7 @@ const Hero = () => {
 
   const viewResume = () => {
     // Open the resume PDF in a new tab with the correct base URL
-    window.open('/SRIVENKATESH.pdf', '_blank');
+    window.open('/SRIVENKATESH R.pdf', '_blank');
   };
 
   return (
